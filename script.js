8fee94104e612810f8f9cf08b17044e0a217f073
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-11-07 11:39:10 +0300

    Add a gameboard module to the script

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
- 
+let Gameboard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+      value = player;
+    };
+    const getValue = () => value;
+
+    return {
+      addToken,
+      getValue,
+    };
+  }
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const dropToken = (row, column, player) => {
+    const selectedCell = board[row][column];
+    if (!selectedCell) return;
+    selectedCell.addToken(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.table(boardWithCellValues);
+  };
+
+  return { getBoard, dropToken, printBoard };
+})();
+
+// console.log(Gameboard.getBoard());
+
+// Gameboard.printBoard();
