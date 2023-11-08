2829a5952be3d9372ec5704f87d2e72e34aec7f8
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-11-08 08:43:15 +0300

    decoupled the Game function(too much function inside function)

--- script.js
+++ script.js
@@ -1,102 +1,97 @@
-let Game = (function () {
-  let Gameboard = (function () {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+let Gameboard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-    function Cell() {
-      let value = 0;
+  function Cell() {
+    let value = 0;
 
-      const addToken = (player) => {
-        value = player;
-      };
-      const getValue = () => value;
-
-      return {
-        addToken,
-        getValue,
-      };
-    }
-
-    for (let i = 0; i < rows; i++) {
-      board[i] = [];
-      for (let j = 0; j < columns; j++) {
-        board[i].push(Cell());
-      }
-    }
-
-    const getBoard = () => board;
-
-    const dropToken = (row, column, player) => {
-      const selectedCell = board[row][column];
-      if (!selectedCell) return;
-      selectedCell.addToken(player);
+    const addToken = (player) => {
+      value = player;
     };
+    const getValue = () => value;
 
-    const printBoard = () => {
-      const boardWithCellValues = board.map((row) =>
-        row.map((cell) => cell.getValue())
-      );
-      console.table(boardWithCellValues);
-    };
-
-    return { getBoard, dropToken, printBoard };
-  })();
-
-  // console.log(Gameboard.getBoard());
-  // Gameboard.printBoard();
-
-  let GameController = (function (
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-  ) {
-    const board = Gameboard;
-
-    const players = [
-      {
-        name: playerOneName,
-        token: "x",
-      },
-      {
-        name: playerTwoName,
-        token: "o",
-      },
-    ];
-
-    let activePlayer = players[0];
-
-    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-    const getActivePlayer = () => activePlayer;
-
-    const printNewRound = () => {
-      board.printBoard();
-      console.log(`${getActivePlayer().name}'s turn.`);
-    };
-
-    const playRound = (row, column) => {
-      console.log(
-        `Dropping ${getActivePlayer().name}'s token into column ${column}...`
-      );
-      board.dropToken(row, column, getActivePlayer().token);
-      switchPlayerTurn();
-      printNewRound();
-    };
-
-    // Initial play game message
-    printNewRound();
-
-    // For the console version, we will only use playRound, but we will need
-    // getActivePlayer for the UI version, so I'm revealing it now
     return {
-      playRound,
-      getActivePlayer,
+      addToken,
+      getValue,
     };
-  })();
+  }
 
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const dropToken = (row, column, player) => {
+    const selectedCell = board[row][column];
+    if (!selectedCell) return;
+    selectedCell.addToken(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.table(boardWithCellValues);
+  };
+
+  return { getBoard, dropToken, printBoard };
+})();
+
+// console.log(Gameboard.getBoard());
+// Gameboard.printBoard();
+
+let GameController = (function (
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard;
+
+  const players = [
+    {
+      name: playerOneName,
+      token: "x",
+    },
+    {
+      name: playerTwoName,
+      token: "o",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `Dropping ${getActivePlayer().name}'s token into column ${
+        column + 1
+      } row ${row + 1}...`
+    );
+    board.dropToken(row, column, getActivePlayer().token);
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // Initial play game message
+  printNewRound();
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
   return {
-    Gameboard,
-    GameController,
+    playRound,
+    getActivePlayer,
   };
 })();
